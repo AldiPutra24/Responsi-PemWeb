35b1ff460c8ab134390f112348a60c3ccf4b2dee
Aldi Putra_21.11.4369 <96532520+AldiPutra24@users.noreply.github.com>
2023-07-12 23:55:19 +0700

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\PostRequest;
+use App\Http\Requests\postRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
