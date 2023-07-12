05b85b1a966e047011c7243b3ed6656e368a0593
Aldi Putra_21.11.4369 <96532520+AldiPutra24@users.noreply.github.com>
2023-07-12 23:56:09 +0700

    Update FrontController.php

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 
 use Inertia\Response;
-use App\Models\Post;
+use App\Models\post;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
